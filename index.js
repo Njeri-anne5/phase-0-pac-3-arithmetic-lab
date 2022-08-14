dc8ec5e22220084060e203bc581185a7b95438ae
+ <+>
2022-08-13 22:16:39 -0400

    Done

---
+++ index.js
@@ -0,0 +1,35 @@
+
+var a,b
+a=8000
+b=1000
+  
+function add(a,b){
+    return a+b;
+
+}
+function subtract(){
+    return a-b;
+
+}
+function multiply(){
+    return a*b;
+
+}
+function divide(){
+    return a/b;
+
+}
+let n=5;
+function increment (){
+    return(n++);
+}
+
+function decrement () {
+    return(n--);
+}
+function makeInt(){
+    return parseInt("n",10);
+}
+function preserveDecimal(){
+    return parseFloat("n");
+}
