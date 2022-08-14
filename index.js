0f0fdecc0bbc72e00a9913f0647a3f04704141de
+ <+>
2022-08-13 22:31:09 -0400

    Done

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 
-var a,b
-a=8000
-b=1000
+var a,b,n;
+
   
 function add(a,b){
-    return a+b;
+    return (a+b);
 
 }
-function subtract(){
-    return a-b;
+function subtract(a,b){
+    return (a-b);
 
 }
-function multiply(){
-    return a*b;
+function multiply(a,b){
+    return (a*b);
 
 }
-function divide(){
-    return a/b;
+function divide(a,b){
+    return (a/b);
 
 }
-let n=5;
-function increment (){
-    return(n++);
+
+function increment (n){
+    return(++n);
 }
 
-function decrement () {
-    return(n--);
+function decrement (n) {
+    return(--n);
 }
-function makeInt(){
-    return parseInt("n",10);
+function makeInt(n){
+    return parseInt(n,10);
 }
-function preserveDecimal(){
-    return parseFloat("n");
+function preserveDecimal(n){
+    return parseFloat(n);
 }
